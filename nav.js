c5359f0f0becfcb2b10e2582287c60e7af425a94
The Code Designer <gazdagbal@gmail.com>
2024-02-21 15:58:51 +0100

    Scroll Reveal animations

--- nav.js
+++ nav.js
@@ -44,16 +44,16 @@ let scrollTimeout;
 document.addEventListener("scroll", () => {
   const visibility = primaryNav.getAttribute('data-visible');
   
-  // Töröld az előző timeout-ot, ha van
+
   clearTimeout(scrollTimeout);
   
   if (visibility === 'false' && window.scrollY > 0) {
-    // Állíts be egy új timeout-ot
+   
     scrollTimeout = setTimeout(() => {
       header.classList.add("scrolled");
     }, 200);
   } else {
-    // Ha felfelé görgetsz, vagy a mobile-nav nyitva van, akkor azonnal töröld a "scrolled" class-t
+   
     header.classList.remove("scrolled");
   }
 });
@@ -70,15 +70,60 @@ window.addEventListener("resize", () => {
 //-------------------------------------------------------
 
 //Scroll Reveal Animations 
-ScrollReveal().reveal('.hero', { duration: 1500, reset: true ,distance: "100px"})
-ScrollReveal().reveal('.divider', { duration: 1500, reset: true,distance: "100px"})
-ScrollReveal().reveal('.message', { duration: 1500 , reset: true,distance: "100px"})
-ScrollReveal().reveal('.features', { duration: 1500 , reset: true,distance: "100px"})
-ScrollReveal().reveal('.card__container', { duration: 1500 , reset: true,distance: "100px"})
-ScrollReveal().reveal('.about', { duration: 1500 , reset: true,distance: "100px"})
-ScrollReveal().reveal('.try-it', { duration: 1500 , reset: true,distance: "100px"})
-ScrollReveal().reveal('.api', { duration: 1500 , reset: true,distance: "100px"})
-ScrollReveal().reveal('.github', { duration: 1500 , reset: true,distance: "100px"})
-ScrollReveal().reveal('.footer__container', { duration: 1500 , reset: true,distance: "100px"})
-ScrollReveal().reveal('.footer__copyright', { duration: 1500 , reset: true,distance: "100px"})
+
+const scrollOptions = {
+  duration: 700,distance: "100px", easing: 'ease-in-out',
+}
+// Hero Section
+ScrollReveal().reveal('.hero__right', { ...scrollOptions, delay: 500,})
+ScrollReveal().reveal('.hero__left h3', {...scrollOptions, origin: 'left', delay: 1000})
+ScrollReveal().reveal('.hero__left h1', {...scrollOptions, origin: 'left', delay: 1500})
+ScrollReveal().reveal('.hero__left p', {...scrollOptions, origin: 'left', delay: 2000})
+ScrollReveal().reveal('.hero__left button', {...scrollOptions, delay: 2500})
+
+
+// Divider Section
+ScrollReveal().reveal('.service-container--1', { ...scrollOptions, origin: 'left', delay: 500})
+ScrollReveal().reveal('.service-container--2', { ...scrollOptions, origin: 'left', delay: 1000})
+ScrollReveal().reveal('.service-container--3', { ...scrollOptions, origin: 'left', delay: 1500})
+
+
+// Message Section 
+ScrollReveal().reveal('.message', { ...scrollOptions, delay: 500})
+
+// Card Section
+ScrollReveal().reveal('.card-1', { ...scrollOptions, delay: 500, mobile: false})
+ScrollReveal().reveal('.card-2', { ...scrollOptions, delay: 1000, mobile: false})
+ScrollReveal().reveal('.card-3', { ...scrollOptions, delay: 1500, mobile: false})
+
+ScrollReveal().reveal('.card-1', { ...scrollOptions, delay: 100, mobile: true})
+ScrollReveal().reveal('.card-2', { ...scrollOptions, delay: 200, mobile: true})
+ScrollReveal().reveal('.card-3', { ...scrollOptions, delay: 300, mobile: true})
+
+
+
+// About Section
+ScrollReveal().reveal('.about__illustration-container', { ...scrollOptions, origin: 'left', delay: 300})
+ScrollReveal().reveal('.about__text-container h2', { ...scrollOptions, origin: 'right', delay: 600})
+ScrollReveal().reveal('.about__text-container p', { ...scrollOptions, origin: 'right', delay: 900})
+ScrollReveal().reveal('.about__text-container img', { ...scrollOptions, delay: 1500})
+
+//try it section
+ScrollReveal().reveal('.try-it__text-container h2', { ...scrollOptions, origin: 'left', delay: 500})
+ScrollReveal().reveal('.try-it__text-container p', { ...scrollOptions, origin: 'left', delay: 1000})
+ScrollReveal().reveal('.try-it__text-container img', { ...scrollOptions, delay: 1500})
+ScrollReveal().reveal('.try-it__illustration-container', { ...scrollOptions, delay: 1500})
+
+//API Section
+ScrollReveal().reveal('.api__illustration-container', { ...scrollOptions, origin: 'left', delay: 500})
+ScrollReveal().reveal('.api__text-container h2', { ...scrollOptions, origin: 'right', delay: 1000})
+ScrollReveal().reveal('.api__text-container p', { ...scrollOptions, origin: 'right', delay: 1500})
+ScrollReveal().reveal('.api__text-container img', { ...scrollOptions, delay: 2000})
+
+//github section
+ScrollReveal().reveal('.github-banner h2', { ...scrollOptions, origin: 'left', delay: 500})
+ScrollReveal().reveal('.github-banner .github-buttons', { ...scrollOptions, origin: 'right', delay: 1000})
+
+//footer section
+ScrollReveal().reveal('.footer__container', { ...scrollOptions,delay: 500})
 //--------------------------------------------------------
